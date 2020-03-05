fadebc4a1a014f62cafba8ed0f3f47baa069e295
Jian Wei Tay <jian.tay@colorado.edu>
2020-03-05 14:39:06 -0700

    Updated sample code. Added PDF of presentation

--- sampleCode.m
+++ sampleCode.m
@@ -1,6 +1,8 @@
 %This example shows how to segment (identify) individual cells and their
 %nuclei.
 
+clearvars %Clear variables in the workspace
+
 %Read image of cell bodies
 I = imread('cells.tif');
 
@@ -9,22 +11,27 @@ mask = imbinarize(I);
 mask = imfill(mask, 'holes');
 
 mask = bwareaopen(mask, 100);
-imshowpair(I, mask, 'montage');  %Display the mask of the image
+imshowpair(I, mask);  %Display the mask on the image
 
 %Measure length of the cells
-celldata = regionprops(mask, 'MajorAxisLength');
+celldata = regionprops(mask, I, 'MajorAxisLength', 'MeanIntensity');
 
 %Number of cells
 numCells = numel(celldata)
 
 %Average cell length
-avgCellLen = mean([celldata.MajorAxisLength]) .* 0.4596
+avgCellLen = mean(cat(1, celldata.MajorAxisLength)) .* 0.4596
 
 %Plot a histogram
-histogram([celldata.MajorAxisLength], 30)
+histogram(cat(1, celldata.MajorAxisLength), 30)
 
-%Combine with image of nuclei for color
+%Plot the mean intensities
+figure;
+plot(cat(1, celldata.MeanIntensity))
+
+
+%Additional: Combine with image of nuclei for color
+figure;
 Inucl = imread('nuclei.tif');
-
 Irgb = cat(3, I, Inucl, zeros(size(I)));
 imshow(Irgb)
